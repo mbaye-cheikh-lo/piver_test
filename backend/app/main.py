fc1c30c0bf4960c9801b6c6f0ce89e88a9b9b77e
mbaye-cheikh-lo <cheikh-lo.mbaye@laplateforme.io>
2026-06-11 05:03:19 +0200

    Test the API with POST events

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,38 +1,23 @@
 from fastapi import FastAPI
 from database import get_connection
 from pydantic import BaseModel
-from datetime import datetime
+
+app = FastAPI()
 
 class Event(BaseModel):
     user_id: str
     type: str
 
-app = FastAPI()
-"""
-@app.get("/events")
-def get_events():
-
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM events")
-    rows = cursor.fetchall()
-
-    cursor.close()
-    conn.close()
-
-    return rows
-"""
 
 @app.post("/events")
-def create_event(user_id: str, type: str):
+def create_event(event: Event):
 
     conn = get_connection()
     cursor = conn.cursor()
 
     cursor.execute(
         "INSERT INTO events (user_id, type) VALUES (%s, %s)",
-        (user_id, type)
+        (event.user_id, event.type)
     )
 
     conn.commit()
@@ -40,3 +25,5 @@ def create_event(user_id: str, type: str):
     conn.close()
 
     return {"message": "Event created"}
+
+
